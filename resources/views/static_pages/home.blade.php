10a8f93059ed0482553bb1325a073d7fbc6c170a
Refive <refive8@gmail.com>
2018-08-24 13:06:01 +0000

    Finish layout and routes

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,6 +6,6 @@
 		<h1>Hello Laravel</h1>
 		<p class="lead">你现在看到的是<a href="#">Laravel入门教程</a>的示例项目主页</p>
 		<p>一切，从这里开始</p>
-		<p><a href="#" class="btn btn-lg btn-success" role='button'>现在注册</a></p>
+		<p><a href="{{route('signup')}}" class="btn btn-lg btn-success" role='button'>现在注册</a></p>
 	</div>
 @stop
